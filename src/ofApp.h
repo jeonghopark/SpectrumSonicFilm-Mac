07c73546b851dad6a68531b645a3dc15c85e2738
jeonghopark <jeonghopark@jeonghopark.de>
2015-11-03 22:51:43 +0100

    first setting

---
+++ src/ofApp.h
@@ -0,0 +1,90 @@
+// http://www.beachfrontbroll.com/2014/02/SnowSpecialEffect.html
+//https://github.com/dfeles/refreq
+
+
+#pragma once
+#include "ofMain.h"
+#include "ofxGui.h"
+
+#define BUFFER_SIZE 8192
+#define SAMPLE_RATE 44100
+#define INITIAL_BUFFER_SIZE 512
+#define BIT 512
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    
+    void audioRequested(float * output, int bufferSize, int nChannels);
+    
+    ofVideoPlayer moviePlay;
+
+    ofDirectory dir;
+    vector<ofVideoPlayer> movies;
+
+    float line;
+    
+    int	maxHertz;
+    int	minHertz;
+    
+    double sineBufferLeft[514];
+    double sineBufferRight[514];
+    
+    float remainderLeft;
+    float remainderRight;
+    float waveLeft;
+    float waveRight;
+    float ampLeft[BIT];
+    float ampRight[BIT];
+    int hertzScaleLeft[BIT];
+    int hertzScaleRight[BIT];
+    float phasesLeft[BIT];
+    float phasesRight[BIT];
+    
+    
+    
+    bool bPlaying;
+    
+    float getPixelLeft(int x, int y);
+    float getAmpLeft(float x, float y);
+    float getFreqLeft(float y);
+    float getPixelRight(int x, int y);
+    float getAmpRight(float x, float y);
+    float getFreqRight(float y);
+    
+    float getAmpLeft(float x, float y, ofPixels _p);
+    
+    
+    ofPixels moviePixel;
+    
+    float scaleCameWidth;
+    
+    float threshold;
+    float screenMiddleX;
+    
+    float camWidth, camHeight;
+    
+    bool fullscreen;
+    
+    ofxPanel gui;
+    ofxButton movieChange;
+    int movieChangeIndex;
+    
+    void moviechangePressed();
+    
+};
